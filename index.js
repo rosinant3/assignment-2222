b06a95c192c1bf52811baeba2d86db01da2c4796
Mirsad Avdic <40442379+rosinant3@users.noreply.github.com>
2022-01-31 02:07:32 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+path = require('path'),
+app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static('build'));
+
+app.get('/mobile', function (req, res) {
+res.send('see this site on a computer plz')
+})
+
+app.listen(app.get('port'), function(err) {
+if (err) {
+console.log(err);
+} else {
+console.log('Running on port: ' + app.get('port')); }
+});
